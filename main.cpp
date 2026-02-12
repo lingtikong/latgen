a803aadc82d50b1add867aa7dbaa2b2d8458450d
LT Kong <konglt@sjtu.edu.cn>
2026-02-12 12:13:45 +0800

    update help info.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ void help()
   printf("The code is driven by menu, please follow the instructions step by step.\n");
   printf("By default, a LAMMPS data file, a compatible map file for fix-phonon, and\n");
   printf("an xyz format file will be written.\n");
-  printf("\nUsage:\n    latgen [-poscar] [-h]\n");
-  printf("\nOptions:\n      -poscar : to output VASP POSCAR instead of LAMMPS data file.\n");
+  printf("\nUsage:\n    latgen [options]\n");
+  printf("\nAvailable options:\n      -poscar : to output VASP POSCAR instead of LAMMPS data file.\n");
   printf("      -q      : to output atomic_style of charge with all q = 0.\n");
-  printf("      -s      : to write all user input to script.inp.\n");
+  printf("      -s      : to write all user input to script.inp, facilitating scripting.\n");
   printf("      -h      : to print this hlep info.\n");
   printf("\nAuthor: Lingti Kong, konglt@sjtu.edu.cn\n(C) 2026\n\n");
 
